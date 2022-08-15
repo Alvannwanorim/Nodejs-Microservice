7a7721dbb9b6ff21580f00c9814bc705b3e89dee
alvannwanorim <alvannwanorim@gmail.com>
2022-08-15 01:16:12 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=alvan2327
+export POSTGRES_HOST=alvandb.c56vg5ee7j5h.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=alvandb
+export AWS_BUCKET=alvan1970339bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
